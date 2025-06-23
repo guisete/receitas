87a1d5aeb965181b246a79ed843bab6f272d063b
guilan machado <guilanmachado85@gmail.com>
2025-06-23 02:14:42 +0000

    Ok tente colocar essa imagem em png no lugar do icon: https://i.imgur.co

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,16 @@
-import { Leaf } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 export function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <Leaf className="h-8 w-8 text-primary" />
+      <Image
+        src="https://i.imgur.com/yfUdrmT.png"
+        alt="Ana Receitas Logo"
+        width={32}
+        height={32}
+        className="h-8 w-8"
+      />
       <span className="text-2xl font-extrabold tracking-tight text-foreground font-headline">
         Ana Receitas
       </span>
