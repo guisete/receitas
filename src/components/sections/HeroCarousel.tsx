b990a16b8594acd3c1cb09da378f3524fddd289a
guilan machado <guilanmachado85@gmail.com>
2025-06-23 19:23:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/sections/HeroCarousel.tsx
+++ src/components/sections/HeroCarousel.tsx
@@ -15,7 +15,7 @@ import { Card, CardContent } from "@/components/ui/card"
 
 const images = [
   { src: "https://i.imgur.com/P2P9bTp.jpeg", alt: "Mulher feliz comendo uma salada saudável", hint: "woman salad" },
-  { src: "https://i.imgur.com/0TPrdCw.jpeg", alt: "Mulher sorrindo medindo a cintura", hint: "woman measuring" },
+  { src: "https://i.imgur.com/fdABaTB.jpeg", alt: "Prato de comida saudável e colorida", hint: "healthy food" },
 ]
 
 export function HeroCarousel() {
