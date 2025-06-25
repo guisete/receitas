32846bb5f5f191f0c3f5af921b662880e8b3044e
guilan machado <guilanmachado85@gmail.com>
2025-06-25 20:33:33 +0000

    Embaixo da sessão do carrossel, adicione uma sessão para mim colocar uma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { FeaturesSection } from "@/components/sections/FeaturesSection";
 import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
 import { PricingSection } from "@/components/sections/PricingSection";
 import { UrgencyBanner } from "@/components/UrgencyBanner";
+import { VslSection } from "@/components/sections/VslSection";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       </div>
       <main className="flex-1">
         <HeroSection />
+        <VslSection />
         <TestimonialsSection />
         <FeaturesSection />
         <PricingSection />
